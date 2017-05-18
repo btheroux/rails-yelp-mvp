76b590571c4a32c2d83bbf85bca8f97dc8b9a1cc
(Brett Theroux) bretttheroux@gmail.com <bretttheroux@gmail.com>
2017-05-18 12:00:31 +0200

    day 28 - exercise done

---
+++ app/models/review.rb
@@ -0,0 +1,8 @@
+class Review < ApplicationRecord
+  belongs_to :restaurant
+
+  validates :content, presence: true
+  validates :rating, presence: true, inclusion: {in: [0,1,2,3,4,5], allow_nil: false}
+  validates_numericality_of :rating, only_integer: true
+  validates :restaurant_id, presence: true
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,92 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         "Fu Hao",
+    address:      "Langgade 1809 Copenhagen",
+    phone_number: "00000000",
+    category:     "chinese"
+  },
+  {
+    name:         "Nakama Izakaya",
+    address:      "Solvej 62 Copenhagen",
+    phone_number: "00000000",
+    category:     "japanese"
+  },
+  {
+    name:         "Mama Mia",
+    address:      "Strøget 231 Copenhagen",
+    phone_number: "00000000",
+    category:     "italian"
+  },
+  {
+    name:         "Royal Garden",
+    address:      "Gladvej 19 Copenhagen",
+    phone_number: "00000000",
+    category:     "chinese"
+  },
+  {
+    name:         "Oui Oui",
+    address:      "Ahornsgade 15 Copenhagen",
+    phone_number: "00000000",
+    category:     "french"
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
+
+puts 'Creating reviews...'
+reviews_attributes = [
+  {
+    content:      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium voluptas quisquam sequi reiciendis itaque ipsum distinctio eos, pariatur architecto nam provident quas id omnis doloribus assumenda tempora officia laborum dolore.",
+    rating:       4,
+    restaurant:   Restaurant.first
+  },
+  {
+    content:      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia eum fugit ut, ea perferendis totam deleniti at, qui accusamus quas amet quisquam quasi adipisci explicabo error! Ipsa fugit, amet obcaecati!",
+    rating:       3,
+    restaurant:   Restaurant.first
+  },
+  {
+    content:      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor saepe obcaecati, incidunt possimus illum reiciendis laborum labore quae cupiditate beatae quibusdam, ea tempora molestias pariatur tempore ex dolorem qui aliquid.",
+    rating:       5,
+    restaurant:   Restaurant.second
+  },
+  {
+    content:      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, reprehenderit ut voluptas, porro, perferendis voluptate expedita iste beatae molestias inventore amet libero aliquam. Quas, iste sequi laudantium. Id, accusantium quasi!",
+    rating:       4,
+    restaurant:   Restaurant.second
+  },
+  {
+    content:      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat explicabo nostrum officia beatae nobis totam sed veritatis ducimus ratione nisi eius nam culpa, provident in corporis rerum repellat. Totam, cumque.",
+    rating:       3,
+    restaurant:   Restaurant.third
+  },
+  {
+    content:      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius aperiam, labore impedit harum iste repellat doloremque quibusdam inventore quod nesciunt nihil magnam vero accusantium quae ab corrupti, sit ipsam sint.",
+    rating:       2,
+    restaurant:   Restaurant.third
+  },
+  {
+    content:      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil natus distinctio iusto autem voluptate itaque id atque, qui minima repudiandae commodi temporibus tempore enim error aliquid consequuntur cum unde harum.",
+    rating:       5,
+    restaurant:   Restaurant.fourth
+  },
+  {
+    content:      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit magnam dolores praesentium id nisi eos nemo. Explicabo quia aut, rem sapiente pariatur minus odit, hic dolorum veritatis temporibus dolorem ipsa.",
+    rating:       5,
+    restaurant:   Restaurant.fourth
+  },
+  {
+    content:      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam perspiciatis dolore nisi esse ut modi enim omnis necessitatibus, ipsa quis sint, distinctio est amet dolor suscipit facilis, commodi molestiae ipsum.",
+    rating:       4,
+    restaurant:   Restaurant.last
+  }
+]
+Review.create!(reviews_attributes)
+puts 'Finished!'
